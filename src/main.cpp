5dd5703ef644b97818a1be9adea3667ad24e2cd9
Dash1Nova <zdanovicdarja@gmail.com>
2026-02-07 21:23:01 +0200

    Added struct and user interaction

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 
+struct Student {
+    std::string name, surname;
+    int n, nd, egz, sum;
+    float final_mark, avg;
+};
+
 int main()
 {
-    std::cout << "Hello world!" << std::endl;
+    Student s;
+    std::cout << "Iveskite studento varda ir pavarde: " << std::endl;
+    std::cin >> s.name >> s.surname;
     return 0;
 }
