d9e1226ee6322eac3bd8725e6228f539e1f13e2f
Dash1Nova <zdanovicdarja@gmail.com>
2026-02-07 21:42:07 +0200

    Version with avg and required output

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,13 @@ struct Student {
 int main()
 {
     Student s;
-    std::cout << "Iveskite studento varda ir pavarde: " << std::endl;
-    std::cin >> s.name >> s.surname;
+    std::cout << "Iveskite studento varda: " << std::endl;
+    std::cin >> s.name;
+    std::cout << "Iveskite studento pavarde: " << std::endl;
+    std::cin >> s.surname;
     std::cout << "Iveskite namu darbu skaiciu: " << std::endl;
     std::cin >> s.n;
+    s.sum = 0;
     for (int i = 0; i < s.n; i++) {
         std::cout << "Iveskite " << i + 1 << " darbo ivertinima: " << std::endl;
         std::cin >> s.nd;
@@ -22,6 +25,9 @@ int main()
     std::cout << "Iveskite egzamino ivertinima: " << std::endl;
     std::cin >> s.egz;
     s.final_mark = 0.4*s.avg + 0.6*s.egz;
-    
+    std::cout << " " << std::endl;
+    std::cout << "Vardas\tPavarde\tGalutinis (Vid.)" << std::endl;
+    std::cout << "----------------------------------------------------" << std::endl;
+    std::cout << s.name << "\t" << s.surname << "\t" << s.final_mark << std::endl;
     return 0;
 }
