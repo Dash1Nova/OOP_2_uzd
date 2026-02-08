065580593db0233fceda0b680ffb603e349ff292
Dash1Nova <zdanovicdarja@gmail.com>
2026-02-09 00:48:21 +0200

    Added median and finally debugged

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,88 @@
 #include <iostream>
 #include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <limits>
 
 struct Student {
     std::string name, surname;
-    int n, nd, egz, sum;
-    double final_mark, avg;
+    int n, egz;
+    std::vector<int> nd;
 };
 
 int main()
 {
     Student s;
+    int mark;
+
+    int sum, output;
+    double avg, median, final_mark;
+    
     std::cout << "Iveskite studento varda: \n";
     std::cin >> s.name;
     std::cout << "Iveskite studento pavarde: \n";
     std::cin >> s.surname;
     std::cout << "Iveskite namu darbu skaiciu: \n";
     std::cin >> s.n;
-    while (s.n <= 0) {
+    while (std::cin.fail() || s.n <= 0) {
         std::cout << "Klaidinga ivestis. Iveskite teigiama skaiciu.\n";
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         std::cin >> s.n;
     }
-    s.sum = 0;
+
+    sum = 0;
     for (int i = 0; i < s.n; i++) {
         std::cout << "Iveskite " << i + 1 << " darbo ivertinima: \n";
-        std::cin >> s.nd;
-        while (s.nd <= 0 || s.nd > 10) {
-        std::cout << "Klaidinga ivestis. Iveskite skaiciu nuo 1 iki 10 imtinai.\n";
-        std::cin >> s.nd;
+        std::cin >> mark;
+        while (std::cin.fail() || mark <= 0 || mark > 10) {
+            std::cout << "Klaidinga ivestis. Iveskite skaiciu nuo 1 iki 10 imtinai.\n";
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cin >> mark;
         }
-        s.sum += s.nd;
+        s.nd.push_back(mark);
+        sum += mark;
     }
-    s.avg = (double)s.sum/s.n;
+
     std::cout << "Iveskite egzamino ivertinima: \n";
     std::cin >> s.egz;
-    while (s.egz <= 0 || s.egz > 10) {
+    while (std::cin.fail() || s.egz <= 0 || s.egz > 10) {
         std::cout << "Klaidinga ivestis. Iveskite skaiciu nuo 1 iki 10 imtinai.\n";
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         std::cin >> s.egz;
         }
-    s.final_mark = 0.4*s.avg + 0.6*s.egz;
-    std::cout << " " << std::endl;
-    std::cout << "Vardas\tPavarde\tGalutinis (Vid.)\n";
-    std::cout << "----------------------------------------------------\n";
-    std::cout << s.name << "\t" << s.surname << "\t" << std::fixed << std::setprecision(2) << s.final_mark << "\n";
+    
+    std::cout << "Kaip skaiciuoti galutini pazymi: naudojant vidurki - iveskite 0, naudojant mediana - iveskite 1: \n";
+    std::cin >> output;
+    while (std::cin.fail() || (output != 0 && output != 1)) {
+        std::cout << "Klaidinga ivestis. Iveskite 0 arba 1:\n";
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cin >> output;
+        }
+
+        std::cout << " " << std::endl;
+        
+        if (output == 0) {
+            avg = (double)sum/s.n;
+            final_mark = 0.4*avg + 0.6*s.egz;
+            std::cout << "Vardas\tPavarde\tGalutinis (Vid.)\n";
+            std::cout << "----------------------------------------------------\n";
+            std::cout << s.name << "\t" << s.surname << "\t" << std::fixed << std::setprecision(2) << final_mark << "\n";
+        } else {
+            std::sort(s.nd.begin(), s.nd.end());
+            if (s.n % 2 == 1) {
+                median = s.nd.at(s.n/2);
+            } else {
+                median = (s.nd.at(s.n/2 - 1) + s.nd.at(s.n/2))/2.0;
+            }
+            final_mark = 0.4*median + 0.6*s.egz;
+            std::cout << "Vardas\tPavarde\tGalutinis (Med.)\n";
+            std::cout << "----------------------------------------------------\n";
+            std::cout << s.name << "\t" << s.surname << "\t" << std::fixed << std::setprecision(2) << final_mark << "\n";
+    }
+
     return 0;
 }
