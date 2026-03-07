6b86379683b9bd3fb919501871d566691ad8d044
Dash1Nova <zdanovicdarja@gmail.com>
2026-03-08 01:33:40 +0200

    corrections 3.0

--- src/fileHandling.cpp
+++ src/fileHandling.cpp
@@ -5,7 +5,6 @@
 #include <sstream>
 #include <iostream>
 #include <iomanip>
-#include <random>
 
 bool createFile (const std::vector<Student> &Students, int n) {
     try {
@@ -80,66 +79,6 @@ bool readFile(const std::string &filename, std::vector<Student> &Students) {
 
     } catch (const std::exception& e) {
         std::cerr << "Failo atidarymo klaida: " << e.what() << std::endl;
-        return false;
-    }
-    catch (...) {
-        std::cerr << "Nezinoma klaida." << std::endl;
-        return false;
-    }
-}
-
-bool generateFile(const std::string &filename) {
-    try {
-    int ndCount = 5;
-    int fnumb;
-    long long records;
-    
-    fnumb = inputInt("Kiek failu generuoti: \n", 1);
-
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> name;
-    std::uniform_int_distribution<> grade(1, 10);
-
-        for (int f = 0; f < fnumb; f++) {
-
-            records = inputInt("Kokio dydzio faila generuoti? Iveskite irasu skaiciu:\n", 1);
-            std::string filename = "studentai_" + std::to_string(records) + ".txt";
-
-            std::ofstream file(filename);
-
-            if (!file.is_open()) {
-                throw std::runtime_error("Nepavyko sukurti failo.");
-            }
-
-            file << std::left << std::setw(15) << "Vardas" << std::setw(15) << "Pavarde";
-
-            for (int i = 0; i < ndCount; i++) {
-                file << std::setw(6) << ("ND" + std::to_string(i + 1));
-            }
-
-            file << std::setw(9) << "Egzaminas.\n";
-
-            name = std::uniform_int_distribution<>(1, records);
-
-            for (long long i = 0; i < records; i++) {
-
-                int newName = name(gen);
-                int newSurname = name(gen);
-
-                file << std::left << std::setw(15) << ("Vardas" + std::to_string(newName)) << std::setw(15) << ("Pavarde" + std::to_string(newSurname));
-
-                for (int j = 0; j < ndCount; j++) {
-                    file << std::setw(6) << grade(gen);
-                }
-
-                file << std::setw(10) << grade(gen) << "\n";
-            }
-        }
-    return true;
-}
-    catch (const std::exception &e) {
-        std::cerr << "Klaida: " << e.what() << std::endl;
         return false;
     }
     catch (...) {
