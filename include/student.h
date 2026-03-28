d81ee5280f2872de231568ac57e29d0292953fef
Dash1Nova <zdanovicdarja@gmail.com>
2026-03-28 14:36:53 +0200

    corected for classes 5.0

--- include/student.h
+++ include/student.h
@@ -4,11 +4,34 @@
 #include <string>
 #include <vector>
 
-struct Student {
-    std::string name, surname;
-    int egz;
-    std::vector<int> nd;
-    double finalAvg, finalMed;
-};
+class Student {
+private:
+    std::string name_;
+    std::string surname_;
+    std::vector<int> nd_;
+    int egz_;
+
+    double finalAvg_;
+    double finalMed_;
+
+    void calculate();
+
+public:
+    Student() = delete;
+    Student(const std::string& name, const std::string& surname, const std::vector<int>& nd, int egz);
+
+        const std::string& getName() const;
+        const std::string& getSurname() const;
+        const std::vector<int>& getNd() const;
+        int getEgz() const;
+
+        double getFinalAvg() const;
+        double getFinalMed() const;
+
+        void setNd(const std::vector<int>& nd);
+        void setEgz(int egz);
+
+        void addNd(int mark);
+    };
 
 #endif
