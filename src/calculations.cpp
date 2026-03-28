654f9a67d9f09594e16f7fdcdff887f850e433e1
Dash1Nova <zdanovicdarja@gmail.com>
2026-03-28 14:24:40 +0200

    switched to classes

--- src/calculations.cpp
+++ src/calculations.cpp
@@ -1,30 +1,14 @@
 #include "calculations.h"
 #include <algorithm>
+#include <random>
 
-double med(const std::vector<int> &nd, int egz) {
-    std::vector<int> temp = nd;
-    if (nd.empty()) return 0.6 * egz;
-    std::sort(temp.begin(), temp.end());
-    double median;
-    if (temp.size() % 2 == 1) {
-        median = temp.at(temp.size()/2);
-    } else {
-        median = (temp.at(temp.size()/2 - 1) + temp.at(temp.size()/2))/2.0;
-    }
-    return 0.4*median + 0.6*egz;
-}
-
-double avg(const std::vector<int> &nd, int egz) {
-    if (nd.empty()) return 0.6*egz;
-    int sum = 0;
-        for (int mark : nd) {
-            sum += mark;
-        }
-    return 0.4*(double(sum)/nd.size()) + 0.6*egz;
-}
 
 std::string GenerateName(const std::string &vardas, const std::vector<std::string> &saknys) {
-    std::string saknis = saknys.at(rand() % saknys.size());
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dist(0, saknys.size() - 1);
+
+    std::string saknis = saknys.at(dist(gen));
 
     if (vardas.substr(vardas.size() - 2) == "as")
         return saknis + "enis";
@@ -36,23 +20,23 @@ std::string GenerateName(const std::string &vardas, const std::vector<std::strin
 }
 
 bool compareByName(const Student &a, const Student &b) {
-    if (a.name.length() != b.name.length()) {
-        return a.name.length() < b.name.length();
+    if (a.getName().length() != b.getName().length()) {
+        return a.getName().length() < b.getName().length();
     }
-    return a.name < b.name;
+    return a.getName() < b.getName();
 }
 
 bool compareBySurname(const Student &a, const Student &b) {
-    if (a.surname.length() != b.surname.length()) {
-        return a.surname.length() < b.surname.length();
+    if (a.getSurname().length() != b.getSurname().length()) {
+        return a.getSurname().length() < b.getSurname().length();
     }
-    return a.surname < b.surname;
+    return a.getSurname() < b.getSurname();
 }
 
 bool compareByMed(const Student &a, const Student &b) {
-    return a.finalMed < b.finalMed;
+    return a.getFinalMed() < b.getFinalMed();
 }
 
 bool compareByAvg(const Student &a, const Student &b) {
-    return a.finalAvg < b.finalAvg;
+    return a.getFinalAvg() < b.getFinalAvg();
 }
