41cce95ff45bafc5c68d8603d9634afda05270b1
Dash1Nova <zdanovicdarja@gmail.com>
2026-04-21 21:50:27 +0300

    corrections for base class and derived class

--- include/student.h
+++ include/student.h
@@ -1,13 +1,12 @@
 #ifndef STUDENT_H
 #define STUDENT_H
 
-#include <string>
+#include "person.h"
 #include <vector>
+#include <iostream>
 
-class Student {
+class Student : public Person {
 private:
-    std::string name_;
-    std::string surname_;
     std::vector<int> nd_;
     int egz_;
 
@@ -19,28 +18,28 @@ private:
 public:
     Student(const std::string name = "Julius", const std::string surname = "Petraitis", const std::vector<int> nd = {1, 2}, int egz = 5);
 
-        ~Student();
+    ~Student();
 
-        Student(const Student& other);
-        Student& operator=(const Student& other);
+    Student(const Student& other);
+    Student& operator=(const Student& other);
 
-        Student(Student&& other) noexcept;
-        Student& operator=(Student&& other) noexcept;
+    Student(Student&& other) noexcept;
+    Student& operator=(Student&& other) noexcept;
 
-        const std::string& getName() const;
-        const std::string& getSurname() const;
-        const std::vector<int>& getNd() const;
-        int getEgz() const;
+    int getEgz() const;
+    const std::vector<int>& getNd() const;
 
-        double getFinalAvg() const;
-        double getFinalMed() const;
+    double getFinalAvg() const;
+    double getFinalMed() const;
 
-        void setNd(const std::vector<int>& nd);
-        void setEgz(int egz);
-        void addNd(int mark);
+    void setNd(const std::vector<int>& nd);
+    void setEgz(int egz);
+    void addNd(int mark);
 
-        friend std::ostream& operator<<(std::ostream& os, const Student& s);
-        friend std::istream& operator>>(std::istream& is, Student& s);
-    };
+    void print() const override;
+
+    friend std::ostream& operator<<(std::ostream& os, const Student& s);
+    friend std::istream& operator>>(std::istream& is, Student& s);
+};
 
 #endif
