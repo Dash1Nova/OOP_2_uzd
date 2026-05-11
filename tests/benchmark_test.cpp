9e3845638f9ed6b07581894b5edb616ff22e2b4c
Dash1Nova <zdanovicdarja@gmail.com>
2026-05-12 00:57:59 +0300

    add tests for comparison

---
+++ tests/benchmark_test.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <iomanip>
+#include "../include/Vector.h"
+
+using namespace std;
+using namespace std::chrono;
+
+void runTest(size_t sz)
+{
+    cout << "Size: " << sz << "\n";
+
+    auto start1 = high_resolution_clock::now();
+
+    std::vector<int> v1;
+    for (size_t i = 0; i < sz; i++)
+        v1.push_back(i);
+
+    auto end1 = high_resolution_clock::now();
+
+    auto timeStd = duration_cast<milliseconds>(end1 - start1).count();
+
+    auto start2 = high_resolution_clock::now();
+
+    Vector<int> v2;
+    for (size_t i = 0; i < sz; i++)
+        v2.push_back(i);
+
+    auto end2 = high_resolution_clock::now();
+
+    auto timeCustom = duration_cast<milliseconds>(end2 - start2).count();
+
+    cout << "std::vector: " << timeStd << " ms\n";
+    cout << "Vector: " << timeCustom << " ms\n";
+    cout << "-----------------------------\n";
+}
+
+int main()
+{
+    runTest(10000);
+    runTest(100000);
+    runTest(1000000);
+    runTest(10000000);
+    return 0;
+}
